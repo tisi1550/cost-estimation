4f499f4648ded4b02323fd1a588e22afc0d8e45c
Tilak Singh <tilaksingh@Tilaks-MacBook-Air.local>
2025-03-16 17:26:48 -0600

    Interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ConfigForm from "./components/ConfigForm";
+import ChatArea from "./components/ChatArea";
 
 function App() {
+  const [chatHistory, setChatHistory] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ConfigForm setChatHistory={setChatHistory} chatHistory={chatHistory} />
+      <ChatArea chatHistory={chatHistory} />
     </div>
   );
 }
